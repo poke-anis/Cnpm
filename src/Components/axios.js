d8fdff43f423b04f5da94a4dbc7d485d352e3f4d
pokeanis <poke-anis@live.fr>
2022-02-15 11:36:16 +0100

    env

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import {Cookies} from 'react-cookie';
 const cookies = new Cookies();
-
+const url = process.env.REACT_APP_BACKEND
 
 
 const instance = axios.create({
   
-    baseURL: 'http://127.0.0.1:3001/api/',
+    baseURL: `${url}/api/`,
     headers: {'Authorization': 'Bearer '+cookies.get('token_key')}
 });
 
