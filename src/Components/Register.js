dc54f71a84b8e796cd5bd071919f49a7120f021c
pokeanis <poke-anis@live.fr>
2022-02-15 14:56:33 +0100

    2

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -475,7 +475,7 @@ function Register(props) {
 
   return (
     <AuthBox>
-      <div style={{width:"30%",height:"100%",borderRight: "1px solid #d8d8d8",paddingLeft:"2%"}}>
+      <div style={{height:"100%",borderRight: "1px solid #d8d8d8",paddingLeft:"2%"}}>
       <label htmlFor="Etes vous ?" >
         S'inscrire en tant que :
         <Form.Check
